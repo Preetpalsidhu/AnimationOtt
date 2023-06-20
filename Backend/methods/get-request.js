6b517e5927fb98572956203509c2c271b422ecf2
Preetpalsidhu <sidhupreetpalsingh21@gmail.com>
2023-06-21 03:48:20 +0530

    backend get post funtions parser

--- Backend/methods/get-request.js
+++ Backend/methods/get-request.js
@@ -1,13 +1,38 @@
-function getReq(req, res){
-    if(req.url === "/api/movies"){
-        res.statusCode = 200;
-        res.setHeader("Content-Type", "application/json");
-        res.write(JSON.stringify(req.movies));
-        res.end();
+function getReq(req, res) {
+  let baseUrl = req.url.substring(0, req.url.lastIndexOf("/") + 1);
+  console.log(baseUrl);
+  let id = req.url.split("/")[3];
+  console.log(id);
+  const regexV4 = new RegExp(/^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/);
+
+  if (req.url === "/api/movies") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.write(JSON.stringify(req.movies));
+    res.end();
+  } else if (!regexV4.test(id)) {
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end(JSON.stringify("Validation failed UUID is not valid"));
+  } else if (baseUrl=="/api/movies/" && regexV4.test(id)) {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    let filterMovie = req.movies.filter((movie) => {
+      return movie.id === id;
+    });
+
+    if(filterMovie.length > 0) {
+      res.statusCode = 200;
+      res.write(JSON.stringify(filterMovie));
+      res.end();
     } else {
-        res.writeHead(404, {"Content-Type":"application/json"});
-        res.end({title: "Not Found", message: "Route not found"});
+      res.statusCode = 404;
+      res.write(JSON.stringify("Movie not found"));
+      res.end();
     }
-};
+  } else {
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end(JSON.stringify("Route not found"));
+  }
+}
 
 module.exports.getReq = getReq;
--- Backend/methods/post-request.js
+++ Backend/methods/post-request.js
@@ -1,1 +1,12 @@
-module.expots = (req,res) => {};
+const crypto = require("crypto");
+const requestBodyparser = require("../util/body-parser");
+module.exports = async (req, res) => {
+    if(req.url === "/api/movies"){
+        try{
+            let body = await requestBodyparser(req);
+            console.log("Request body", req.body);
+        }catch(err){
+            
+        }
+    }
+}
