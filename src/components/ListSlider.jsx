afdb3d6efbf48e34c5d91ef74e944d02f38020fe
Preetpalsidhu <sidhupreetpalsingh21@gmail.com>
2023-06-28 02:36:59 +0530

    slider flex

--- src/components/ListSlider.jsx
+++ src/components/ListSlider.jsx
@@ -3,26 +3,48 @@ import "./listSlider.css";
 
 import { FaAngleLeft } from "react-icons/fa";
 import { FaAngleRight } from "react-icons/fa";
-export default function List(){
-    return(
+import { useRef } from "react";
+
+export default function List() {
+  const listRef = useRef();
+  const clickHandler = (direction) => {
+    let distance = listRef.current.getBoundingClientRect().x;
+    if (direction === "left") {
+      listRef.current.style.transform = `translateX({distance + 230}px)`;
+      alert("left $");
+    }
+    if (direction === "right")
+      listRef.current.style.tranform = `translateX(-230px)`;
+  };
+  return (
     <div className="list">
-        <span className="listTitle">Continue watching</span>
-        <div className="wrapper">
-        <FaAngleLeft/>
-    <div className="listContiner">
-        <ListItem/>
-        <ListItem/>
-        <ListItem/>
-        <ListItem/>
-        <ListItem/>
-        <ListItem/>
-        <ListItem/>
-        <ListItem/>
-        <ListItem/>
-        <ListItem/>
+      <span className="listTitle">Continue watching</span>
+      <div className="wrapper">
+        <FaAngleLeft
+          className="sliderBtn left"
+          onClick={() => {
+            clickHandler("left");
+          }}
+        />
+        <div className="listContiner" ref={listRef}>
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+        </div>
+        <FaAngleRight
+          className="sliderBtn right"
+          onClick={() => {
+            clickHandler("right");
+          }}
+        />
+      </div>
     </div>
-    <FaAngleRight/>
-    </div>
-    </div>
-    );
+  );
 }
