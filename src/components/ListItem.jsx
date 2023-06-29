3b749cfe6c9ffbe502427b16bdb8a4a7c9ca61ad
Preetpalsidhu <sidhupreetpalsingh21@gmail.com>
2023-06-29 07:28:18 +0530

    video hover

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Home from "./Home.webp";
 import "./listItem.css";
 import { FaPlay, FaPlus, FaThumbsUp, FaThumbsDown } from "react-icons/fa";
+import Trailer from "./OnePieceTrailer.mp4";
 
 export default function ListItem({index}){
     const [isHovered, setIsHovered] = useState(false);
@@ -9,6 +10,9 @@ export default function ListItem({index}){
         <div className="listItem" onMouseEnter={() =>setIsHovered(true)} onMouseLeave={() =>setIsHovered(false)}
         style={{ left: isHovered && index * 225 - 50 + index * 2.5 }}>
         <img src={Home} className="cardImg"></img>
+     {isHovered && (
+        <>
+        <video  src={Trailer} autoPlay={true} loop/>
         <div className="itemInfo">
             <div className="icons">
                 <FaPlay className="icon" />
@@ -21,10 +25,11 @@ export default function ListItem({index}){
                 <span className="limit">13+</span>
                 </div>
                 <div className="desc">
-                    <span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque, ratione quasi quod excepturi, sed ab expedita blanditiis aperiam sit aliquid a sequi nulla non, iusto saepe aspernatur! Molestias, iusto inventore.</span>
+                    <span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque, ratione quasientore.</span>
                     </div>
                     <div className="genre"><span>Adventure</span></div>
             </div>
+            </>)}
         </div>
     );
 }
