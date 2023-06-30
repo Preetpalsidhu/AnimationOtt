1437ca0e80b1c29903d738e9e392a4e0ca5fa1cd
Preetpalsidhu <sidhupreetpalsingh21@gmail.com>
2023-06-30 05:34:02 +0530

    express api Authentication jwt token generation

---
+++ api/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+const authRoute = require("./routes/auth");
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    //useCreateIndex: true,
+}).then(() => console.log("DB connection succesful")
+).catch((err) => console.log(err));
+
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
+
+app.listen(8800, ()=>{
+    console.log("Backend server started");
+})
