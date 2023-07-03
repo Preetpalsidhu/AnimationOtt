a7fd928d4e9a8ce617b50443d5bbd381dd630b84
Preetpalsidhu <sidhupreetpalsingh21@gmail.com>
2023-07-03 06:03:20 +0530

    login

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import Home from "./pages/home/Home";
+import Register from "./pages/register/Register";
+import Login from "./pages/login/Login";
 import React from "react";
 import {BrowserRouter as Router, Routes, Route, link} from "react-router-dom";
 
@@ -7,6 +9,8 @@ function App() {
   <Router>
     <Routes>
       <Route path="/" element={<Home/>}> </Route>
+       <Route path="/regiter" element={<Register/>}> </Route>
+        <Route path="/login" element={<Login/>}> </Route>
     </Routes>
   </Router>
   );
